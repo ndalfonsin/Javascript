210a6976ddeebc0c7e081cfaadac6f951aebb6e2
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-07-27 16:44:03 -0300

    upload

--- IpInfo/main.js
+++ IpInfo/main.js
@@ -1,13 +1,38 @@
 const OPTIONS = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key':'71e59e906dmshfdc9ed284df2469p1b599cjsn0355f0f200d5',
-        'X-RapidAPI-Host':'ip-geolocation-and-threat-detection.p.rapidapi.com'
+        'X-RapidAPI-Key': '8b64cb37b8msh635e94b5f80527ap120a46jsn0af55c71ea09',
+		'X-RapidAPI-Host': 'whoisapi-ip-geolocation-v1.p.rapidapi.com'
     }
 }
 
 const fetchIpInfo = ip => {
-    return fetch(`https://ip-geolocation-and-threat-detection.p.rapidapi.com/${ip}`)
+    return fetch(`https://whoisapi-ip-geolocation-v1.p.rapidapi.com/api/v1?ipAddress=${ip}`, OPTIONS)
     .then(res => res.json())
-    .catch(err => console.error(err))
+    .catch(err => console.error(err));
 }
+
+const $form = document.querySelector('#form');
+const $input = document.querySelector('#input');
+const $submit = document.querySelector('#submit');
+const $result = document.querySelector('#result');
+
+$form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const {value} = $input;
+    if (!value) return;
+
+    $submit.setAttribute('disabled', '');
+    $submit.setAttribute('aria-busy', 'true');
+     
+
+    const ipInfo = await fetchIpInfo(value);
+    console.log(ipInfo);
+    
+    if (ipInfo) {
+        $result.innerHTML = JSON.stringify(ipInfo, null, 2);
+    }
+
+    $submit.removeAttribute('disabled');
+    $submit.removeAttribute('aria-busy');
+})
