b2acc0c846065be785117f27f4b86e97fd0b5ff6
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-07-28 21:14:15 -0300

    upload

--- 3-Objects/6-GettersAndSetters.js
+++ 3-Objects/6-GettersAndSetters.js
@@ -14,3 +14,26 @@ SET: Tenga en cuenta lo siguiente al trabajar con setters:
 Debe tener exactamente un parámentro
 
 */
+
+const gato = {
+    nombre: "Valiente",
+    duerme: true,
+    edad: 10,
+    enemigos: ["agua", "perros"],
+    comer(comida) {
+        return `${this.nombre} está comiendo ${comida}`;
+    },
+    get nombreMayuscula() {
+        return this.nombre.toUpperCase();
+    },
+    set nuevoEnemigo(nuevo) {
+        this.enemigos.push(nuevo);
+    },
+};
+
+// GET
+console.log(gato.nombreMayuscula);
+
+// SET
+gato.nuevoEnemigo = "batman";
+console.log(gato.enemigos);
