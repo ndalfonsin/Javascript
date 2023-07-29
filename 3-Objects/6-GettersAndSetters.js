e9b46ca66f46ad6a35b84c1d216192cf95830e15
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-07-28 21:07:31 -0300

    upload

---
+++ 3-Objects/6-GettersAndSetters.js
@@ -0,0 +1,16 @@
+/*
+get: Enlaza la propiedad de un objeto con una función que será llamada cuando la propiedad es buscada.
+
+set: La sintaxis set asocia la propiedad de un objeto a una función que será llamada cuando haya un 
+intento de asignar valor a esa propiedad.
+
+GET: Tenga en cuenta lo siguiente al trabajar con la sintaxis get:
+
+Debe tener exactamente cero parametros.
+No debe haber múltiples getters para una misma propiedad.
+
+SET: Tenga en cuenta lo siguiente al trabajar con setters:
+
+Debe tener exactamente un parámentro
+
+*/
