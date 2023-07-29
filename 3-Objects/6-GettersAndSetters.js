9e311125531264128906dce6386899e5a4711b7d
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-07-28 21:35:16 -0300

    upload

--- 3-Objects/6-GettersAndSetters.js
+++ 3-Objects/6-GettersAndSetters.js
@@ -38,3 +38,48 @@ console.log(gato.nombreMayuscula);
 gato.nuevoEnemigo = "batman";
 console.log(gato.enemigos);
 
+/*
+-por valor: Cuando asignamos valores primitivos (Boolean, Null, Undefined, Number, String y Symbol), el valor asignado es una copia del 
+valor que estamos asignando.
+
+-por referencia: Pero cuando asignamos valores NO primitivos o complejos (Object, Array y Function), JavaScript copia “la referencia”, 
+lo que implica que no se copia el valor en sí, si no una referencia a través de la cual accedemos al valor original.
+*/
+
+// Primitivos (por valor)
+
+let a = "hola";
+let b = a;
+console.log(b); //hola
+
+// Si cambiamos el valor de a, b sigue siendo "hola"
+
+a = "chao";
+console.log(b); //hola
+
+// No primitivos: (por referencia)
+
+let c = ["hola"];
+let d = c;
+console.log(d); //hola
+
+let e = ["hola"];
+let f = e; 
+
+f.push("chao");
+
+console.log(f);// ['hola', 'chao']
+
+//Lo mismo pasa con los objetos
+
+const aa = {
+    nombre: "hola",
+};
+
+const bb = aa;
+
+aa.nombre = "chao";
+
+console.log(bb); // { nombre: 'chao'}
+
+
