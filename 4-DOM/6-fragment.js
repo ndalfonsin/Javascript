2b5be24a889455abb4f87e3ffea744f35c98f435
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-07-30 06:33:27 -0300

    upload

---
+++ 4-DOM/6-fragment.js
@@ -0,0 +1,27 @@
+/* FRAGMENT
+
+    -> documentFragment()
+    -> createDocumentFragment()
+
+-La interfaz documentFragment representa un objeto de documento minimo que no tiene padre.
+-Se utiliza como un version ligera de document que almacena un segmento estructura de documento
+compuesta de nodos como un documento estandar
+-la gran dif se debe al hecho de que el fragmento de documento no forma parte de la estructura del 
+documento activo.
+-Los cambios realizados en el fragmento no afrectan al documento (incluso en reflow) ni inciden
+en el rendimiento cuando se realizan cambios
+
+*/
+
+const listaDinamica = document.querySelector("#listaDinamica");
+const arrayElementos = ["Peru", "Bolivia", "Argentina", "Uruguay"];
+
+const fragment = document.createDocumentFragment();
+
+arrayElementos.forEach((pais) => {
+    const li = document.createElement("li")
+    li.textContent = pais;
+    fragment.appendChild(li);
+})
+
+listaDinamica.appendChild(fragment);
