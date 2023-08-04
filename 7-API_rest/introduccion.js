61c14676db18baf5c034834420a69c5a15368b91
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-08-04 01:34:47 -0300

    upload

---
+++ 7-API_rest/introduccion.js
@@ -0,0 +1,32 @@
+/*Una API REST (Representational State Transfer) es un tipo de interfaz de programación de aplicaciones (API)
+que se basa en los principios y convenciones del protocolo HTTP (Hypertext Transfer Protocol). 
+Es una de las arquitecturas más comunes y ampliamente utilizadas para diseñar servicios web que permiten la 
+comunicación entre sistemas y aplicaciones.
+
+En una API REST, los recursos de la aplicación, como datos o funciones, se representan mediante URLs (Uniform Resource Locators) 
+y se accede a ellos utilizando los diferentes métodos HTTP, como 
+    GET, POST, PUT(REEMPLAZAR), PATCH(ACTUALIZAR), DELETE, entre otros. 
+Cada método representa una operación específica que se puede realizar sobre el recurso. 
+
+
+en la carpeta de la api
+
+1- npm init -y (acepto todo lo que viene por defecto)
+
+crea un package.json con:
+
+{
+  "name": "api",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+}
+
+*/
+
