e57bda0bd82f2862770139afc91a22e05d93beef
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-08-06 20:20:08 -0300

    upload

--- 7-API_rest/api/api.js
+++ 7-API_rest/api/api.js
@@ -4,9 +4,13 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+app.use(express.json()) //middleware
+
 
 dotenv.config();
 mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.adsx3wt.mongodb.net/miapp?retryWrites=true&w=majority`)
+
+
 
 app.get("/", user.list) // 200 para devolver datos al cliente
 app.post("/", user.create)
@@ -16,6 +20,11 @@ app.patch('/:id', user.update)
 app.delete('/:id', user.destroy)
 /* vamos a saber a que metodo estamos apuntando cuando llamemos el fetch con el 
 method, especificando si es put, patch o delete */
+
+app.get('*', (req, res) => {
+    res.status(404).send('Esta pagina no existe')
+})
+
 app.listen(port, () => {
     console.log("Arrancando la app")
 })
--- 7-API_rest/api/user.controller.js
+++ 7-API_rest/api/user.controller.js
@@ -1,18 +1,42 @@
+const mongoose = require('mongoose');
+
+const Users = mongoose.model('User', {
+    name: { type: String, required: true, minLength: 3 },
+    lastname: { type: String, required: true, minLength: 3 },
+})
+
+
 const User = {
-    list: (req, res) => {
-        res.status(200).send('Hola mundo!');// 200 para devolver datos al cliente
+    list: async (req, res) => {
+        const users = await Users.find()
+        res.status(200).send(users);// 200 para devolver datos al cliente
     },
-    create: (req, res) => {
-        res.status(201).send('Creando');// 201 se creo exitosamente una entrada
+    create: async (req, res) => {
+        // console.log(req.body) //para esto necesitamos ejecutar un middleware
+        const user = new Users(req.body)
+        const savedUser = await user.save()
+        res.status(201).send(savedUser._id);// 201 se creo exitosamente una entrada
     },
-    update: (req, res) => {
-        res.status(204).send('Actualizando');//solo vamos a hacer el envio del estado
+    update: async (req, res) => {
+        const { id } = req.params
+        const user = await Users.findOne({ _id: id})
+        Object.assign(user, req.body)
+        await user.save()
+        res.status(200)//solo vamos a hacer el envio del estado
     },
-    destroy: (req, res) => {
+    destroy: async (req, res) => {
+        const { id } = req.params
+        const user = await Users.findOne({ _id: id})
+        if (user){
+            await user.deleteOne({ _id: user._id });
+            console.log("Eliminado")
+        }
         res.status(204).send('Destruyendo');//solo vamos a hacer el envio del estado
     },
-    get: (req, res) => {
-        res.status(200).send('Hola sin listar');
+    get: async (req, res) => {
+        const { id } = req.params
+        const user = await Users.findOne({ _id: id})
+        res.status(200).send(user);
     }
 }
 
