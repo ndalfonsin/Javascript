9d0f2e3351bbc2577e05226cc67b3ef1ec81167f
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-08-06 18:40:39 -0300

    upload

--- 7-API_rest/api/api.js
+++ 7-API_rest/api/api.js
@@ -2,25 +2,23 @@ const express = require('express');
 const user = require('./user.controller')
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.adsx3wt.mongodb.net/miapp?retryWrites=true&w=majority`)
 
 app.get("/", user.list) // 200 para devolver datos al cliente
-
 app.post("/", user.create)
-
-app.get('/:id', (req, res) => { //dato variable que va a aparecer en la ruta
-    console.log(req.params)
-    res.sendStatus(200).send(req.params) //solo vamos a hacer el envio del estado
-})
-
+app.get('/:id', (req, res) => { user.get})//dato variable que va a aparecer en la ruta
 app.put('/:id', user.update)
-
 app.patch('/:id', user.update)
-
 app.delete('/:id', user.destroy)
 /* vamos a saber a que metodo estamos apuntando cuando llamemos el fetch con el 
 method, especificando si es put, patch o delete */
-
 app.listen(port, () => {
     console.log("Arrancando la app")
 })
+
+
 
