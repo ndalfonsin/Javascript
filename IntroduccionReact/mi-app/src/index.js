e8c2536d8200de011bb4dc408da4d150fd6e631a
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-08-31 00:21:07 -0300

    upload

--- IntroduccionReact/mi-app/src/index.js
+++ IntroduccionReact/mi-app/src/index.js
@@ -11,9 +11,12 @@ import reportWebVitals from './reportWebVitals';
 //   </React.StrictMode>
 // );
 
-ReactDOM.render(
-  
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
